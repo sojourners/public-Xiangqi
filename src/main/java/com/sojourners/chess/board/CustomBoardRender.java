6baf165749f1d9bd7952f70cf33a6d8ab7e192e5
sojourners <1515295755@qq.com>
2024-08-24 16:48:04 +0800

    fix ui

--- src/main/java/com/sojourners/chess/board/CustomBoardRender.java
+++ src/main/java/com/sojourners/chess/board/CustomBoardRender.java
@@ -4,6 +4,7 @@ import com.sojourners.chess.util.PathUtils;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.image.Image;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,24 +16,24 @@ public class CustomBoardRender extends BaseBoardRender {
     public CustomBoardRender(Canvas canvas) {
         super(canvas);
 
-        this.bgImage = new Image(PathUtils.getJarPath() + "/ui/board.png");
+        this.bgImage = new Image(new File(PathUtils.getJarPath() + "/ui/board.png").toURI().toString());
 
         map = new HashMap<>();
-        map.put('r', new Image(PathUtils.getJarPath() + "/ui/br.png"));
-        map.put('n', new Image(PathUtils.getJarPath() + "/ui/bn.png"));
-        map.put('b', new Image(PathUtils.getJarPath() + "/ui/bb.png"));
-        map.put('a', new Image(PathUtils.getJarPath() + "/ui/ba.png"));
-        map.put('k', new Image(PathUtils.getJarPath() + "/ui/bk.png"));
-        map.put('c', new Image(PathUtils.getJarPath() + "/ui/bc.png"));
-        map.put('p', new Image(PathUtils.getJarPath() + "/ui/bp.png"));
+        map.put('r', new Image(new File(PathUtils.getJarPath() + "/ui/br.png").toURI().toString()));
+        map.put('n', new Image(new File(PathUtils.getJarPath() + "/ui/bn.png").toURI().toString()));
+        map.put('b', new Image(new File(PathUtils.getJarPath() + "/ui/bb.png").toURI().toString()));
+        map.put('a', new Image(new File(PathUtils.getJarPath() + "/ui/ba.png").toURI().toString()));
+        map.put('k', new Image(new File(PathUtils.getJarPath() + "/ui/bk.png").toURI().toString()));
+        map.put('c', new Image(new File(PathUtils.getJarPath() + "/ui/bc.png").toURI().toString()));
+        map.put('p', new Image(new File(PathUtils.getJarPath() + "/ui/bp.png").toURI().toString()));
 
-        map.put('R', new Image(PathUtils.getJarPath() + "/ui/rr.png"));
-        map.put('N', new Image(PathUtils.getJarPath() + "/ui/rn.png"));
-        map.put('B', new Image(PathUtils.getJarPath() + "/ui/rb.png"));
-        map.put('A', new Image(PathUtils.getJarPath() + "/ui/ra.png"));
-        map.put('K', new Image(PathUtils.getJarPath() + "/ui/rk.png"));
-        map.put('C', new Image(PathUtils.getJarPath() + "/ui/rc.png"));
-        map.put('P', new Image(PathUtils.getJarPath() + "/ui/rp.png"));
+        map.put('R', new Image(new File(PathUtils.getJarPath() + "/ui/rr.png").toURI().toString()));
+        map.put('N', new Image(new File(PathUtils.getJarPath() + "/ui/rn.png").toURI().toString()));
+        map.put('B', new Image(new File(PathUtils.getJarPath() + "/ui/rb.png").toURI().toString()));
+        map.put('A', new Image(new File(PathUtils.getJarPath() + "/ui/ra.png").toURI().toString()));
+        map.put('K', new Image(new File(PathUtils.getJarPath() + "/ui/rk.png").toURI().toString()));
+        map.put('C', new Image(new File(PathUtils.getJarPath() + "/ui/rc.png").toURI().toString()));
+        map.put('P', new Image(new File(PathUtils.getJarPath() + "/ui/rp.png").toURI().toString()));
     }
 
     @Override
@@ -44,6 +45,11 @@ public class CustomBoardRender extends BaseBoardRender {
     public void drawCenterText(int pos, int piece, ChessBoard.BoardSize style) {
 
     }
+
+//    @Override
+//    public void drawBoardLine(int pos, int padding, int piece, boolean isReverse, ChessBoard.BoardSize style) {
+//
+//    }
 
     @Override
     public void drawPieces(int pos, int piece, char[][] board, boolean isReverse, ChessBoard.BoardSize style) {
